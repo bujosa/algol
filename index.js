fa2592569eb928a6e65ca5819c6692e52bed9f02
bujosa <davidbujosa@gmail.com>
2023-06-16 02:53:12 -0400

    feat: add example command of create an contract api

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
-import FireFly from '@hyperledger/firefly-sdk';
+import FireFly from "@hyperledger/firefly-sdk";
 
 async function main() {
-  const firefly = new FireFly({ host: 'http://localhost:5000' });
+  const firefly = new FireFly({ host: "http://localhost:5000" });
+
   await firefly.createContractAPI({
-    name: 'my-contract',
-    version: '1.0.0',
-    id: 'my-contract-1',
- } );
+    name: "my-contract",
+    interface: {
+      name: "my-contract",
+      version: "1.0.0",
+      id: "my-contract-1",
+    },
+    location: {
+        address: "0x1234",
+    }
+  });
+}
 
 if (require.main === module) {
   main().catch((err) => console.error(`Error: ${err}`));
